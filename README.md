540dc9811089cb9f7bee97daa96a4eed78ba3e84
sebride4988 <sebride4988@gmail.com>
2021-12-26 15:23:24 +0900

    init(*): 기반 작업

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # fireCMS-analytics
- firebase를 활용하는 CMS의 analytics
+
+ fireCMS의 analytics
+
+## 특징
+
+- 해당 서브모듈은 fireCMS-core가 필요합니다.
+
+## 사용하는 법
+
+`<FireCMSCore>`에 서비스를 추가해준다. 코드를 추가한다.
+
+```typescript
+import Analytics from 'fireCMS/analytics';
+
+...
+
+<FireCMSCore
+  ...
+  services={{
+    analytics: Analytics,
+  }}
+>
+  {props.children}
+</FireCMSCore>
+
+...
+```
---
+++ index.ts
@@ -0,0 +1,25 @@
+import { getAnalytics, Analytics as _Analytics } from '@firebase/analytics';
+import { FirebaseApp } from '@firebase/app';
+
+class Analytics {
+  private static _instance: Analytics;
+  private static _analytics?: _Analytics;
+
+  public static initialize(firebaseApp: FirebaseApp) {
+    this._analytics = getAnalytics(firebaseApp);
+  }
+
+  public static get Analytics() {
+    if (!this._analytics) {
+      throw new Error('Analytics is not initialized');
+    }
+    return this._analytics;
+  }
+
+  public static get Instance() {
+    // Do you need arguments? Make it a regular static method instead.
+    return this._instance || (this._instance = new this());
+  }
+}
+
+export default Analytics;
